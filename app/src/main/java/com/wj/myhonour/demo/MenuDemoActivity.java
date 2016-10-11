8e7497647bd1f2c0eda6d4de150103b55aad9ea7
WangJie581825 <you@example.com>
2016-10-11 16:51:42 +0800

    TitleBar设置优化及设置方法 自定义Toast

---
+++ app/src/main/java/com/wj/myhonour/demo/MenuDemoActivity.java
@@ -0,0 +1,31 @@
+package com.wj.myhonour.demo;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.wj.myhonour.R;
+import com.wj.myhonour.basis.BaseActivity;
+
+public class MenuDemoActivity extends BaseActivity {
+
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, MenuDemoActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_menu_demo;
+    }
+
+    @Override
+    public void setTitleBar() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+}
--- app/src/main/java/com/wj/myhonour/demo/TestActivity.java
+++ app/src/main/java/com/wj/myhonour/demo/TestActivity.java
@@ -1,23 +1,41 @@
 package com.wj.myhonour.demo;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.wj.myhonour.R;
+import com.wj.myhonour.basis.BaseActivity;
 import com.wj.myhonour.demo.marquee.MarQueeDemo;
 
-public class TestActivity extends Activity implements View.OnClickListener {
+public class TestActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-
-        initView();
+    public int getContentViewId() {
+        return R.layout.activity_test;
     }
 
-    private void initView() {
+    @Override
+    public void setTitleBar() {
+        setTitleLeft("返回", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast("点击了“返回”按钮");
+            }
+        });
+
+        setTitleWithId(R.string.test);
+
+        setTitleRight(R.drawable.flower, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void initView() {
+        findViewById(R.id.btn_toast).setOnClickListener(this);
+
         findViewById(R.id.btn_activity).setOnClickListener(this);
 
         findViewById(R.id.btn_dialog).setOnClickListener(this);
@@ -26,11 +44,15 @@ public class TestActivity extends Activity implements View.OnClickListener {
 
         findViewById(R.id.btn_marquee).setOnClickListener(this);
 
+        findViewById(R.id.btn_menu).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn_toast:
+                toast("随机数：" + Math.random() * 10000);
+                break;
             case R.id.btn_activity:
                 ShowActivity.actionStart(this, "test001");
                 break;
@@ -43,6 +65,10 @@ public class TestActivity extends Activity implements View.OnClickListener {
             case R.id.btn_marquee:
                 MarQueeDemo.actionStart(this);
                 break;
+            case R.id.btn_menu:
+                MenuDemoActivity.actionStart(this);
+                break;
         }
     }
+
 }
